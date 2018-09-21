c11a5984e7124a7639d49bb7f7543e1ee08af5a2
ml5om <43390029+ml5om@users.noreply.github.com>
2018-09-22 00:20:09 +0300

    Update ml5om13.js

--- ml5om13.js
+++ ml5om13.js
@@ -14,6 +14,27 @@ client.on('ready', function() {
 
       client.on('ready', () => {
       
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+
       });
 /*
 ////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\
